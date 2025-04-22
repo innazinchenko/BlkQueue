8dd338630199fc943e362908223b34da79c21beb
innazinchenko <spangle_red@inbox.ru>
2025-04-22 22:20:30 +0200

    push and pop was added

--- src/ait/mediation/BlkQueueImpl.java
+++ src/ait/mediation/BlkQueueImpl.java
@@ -1,20 +1,43 @@
 package ait.mediation;
 
+import java.util.LinkedList;
+
 public class BlkQueueImpl<T> implements BlkQueue<T> {
+
+    private final LinkedList<T> queue = new LinkedList<>();
+    private final int maxSize;
+
     public BlkQueueImpl(int maxSize) {
-        // TODO
-        throw new UnsupportedOperationException("Not implemented");
+        this.maxSize = maxSize;
+    }
+
+
+    @Override
+    public synchronized void push(T message) {
+        while (queue.size() >= maxSize) {
+            try {
+                this.wait();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        queue.add(message);
+        this.notifyAll();
     }
 
     @Override
-    public void push(T message) {
-        // TODO
-        throw new UnsupportedOperationException("Not implemented");
+    public synchronized T pop() {
+        while (queue.isEmpty()) {
+            try {
+                this.wait();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        T message = queue.removeFirst();
+        notifyAll();
+        return message;
     }
 
-    @Override
-    public T pop() {
-        // TODO
-        throw new UnsupportedOperationException("Not implemented");
-    }
+
 }
