754d0330d6447d7a2ea4736e1ec6c08209489113
innazinchenko <spangle_red@inbox.ru>
2025-04-23 21:28:23 +0200

    Lock was added

--- src/ait/mediation/BlkQueueImpl.java
+++ src/ait/mediation/BlkQueueImpl.java
@@ -1,11 +1,17 @@
 package ait.mediation;
 
 import java.util.LinkedList;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class BlkQueueImpl<T> implements BlkQueue<T> {
 
     private final LinkedList<T> queue = new LinkedList<>();
     private final int maxSize;
+    private final Lock lock = new ReentrantLock();
+    private final Condition notFull = lock.newCondition();
+    private final Condition notEmpty = lock.newCondition();
 
     public BlkQueueImpl(int maxSize) {
         this.maxSize = maxSize;
@@ -15,29 +21,35 @@ public class BlkQueueImpl<T> implements BlkQueue<T> {
     @Override
     public synchronized void push(T message) {
         while (queue.size() >= maxSize) {
+            lock.lock();
             try {
-                this.wait();
+                while (queue.size() >= maxSize) {
+                    notFull.await();
+                }
+                queue.add(message);
+                notEmpty.signal();
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
+            } finally {
+                lock.unlock();
             }
         }
-        queue.add(message);
-        this.notifyAll();
     }
 
     @Override
     public synchronized T pop() {
-        while (queue.isEmpty()) {
-            try {
-                this.wait();
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
+        lock.lock();
+        try {
+            while (queue.isEmpty()) {
+                notEmpty.await();
             }
-        }
         T message = queue.removeFirst();
-        notifyAll();
+        notFull.signal();
         return message;
+    } catch(InterruptedException e){
+        throw new RuntimeException(e);
+    } finally {
+        lock.unlock();
     }
-
-
+ }
 }
